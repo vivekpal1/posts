fdabcd90cea6dd5e3e7a5765f110fc4d7138af2d
vivek pal <31vivekpal@gmail.com>
2025-05-25 05:20:22 +0000

    collection: 2025

--- src/components/postsList/_postsCollection.ts
+++ src/components/postsList/_postsCollection.ts
@@ -1,4 +1,5 @@
 import { getCollection } from 'astro:content'
 const Posts2023 = await getCollection('2023')
 const Posts2024 = await getCollection('2024')
-export const allPosts = [Posts2024, Posts2023]
+const Posts2025 = await getCollection('2025')
+export const allPosts = [Posts2025, Posts2024, Posts2023,]
