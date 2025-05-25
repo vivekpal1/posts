4a612d93b4397079c5f1b701a4a63a8f09fa24d7
vivek pal <31vivekpal@gmail.com>
2025-05-25 04:35:39 +0000

    update: projects

--- src/components/projectsList/_projectList.ts
+++ src/components/projectsList/_projectList.ts
@@ -9,6 +9,30 @@ type Projects<T> = T[]
 
 export const projects: Projects<projectsType> = [
   {
+    projectName: 'Wind Network',
+    description: 'Distributed network for solana to index and maintain the storage offchain.',
+    repoLink: 'https://github.com/wind-network',
+    previewLink: 'https://windnetwork.ai',
+  },
+  {
+    projectName: 'wIndexer',
+    description: 'Decentralised indexer for Solana.',
+    repoLink: 'https://github.com/wind-network/windexer',
+    previewLink: 'https://windnetwork.ai/windexer',
+  },
+  {
+    projectName: 'IPDM',
+    description: 'Innovating global data management with unprecedented speed and scale.',
+    repoLink: 'https://github.com/ipdm-wg/ipdm',
+    previewLink: 'https://ipdm.tech',
+  },
+  {
+    projectName: 'Wink',
+    description: 'You wanna date? No swaps only bidding!',
+    repoLink: 'https://github.com/vivekpal1/wink',
+    previewLink: 'https://wink.vercel.app',
+  },
+  {
     projectName: 'Senshi',
     description: 'In-game tokenomics and crosschain deposits on solana.',
     repoLink: 'https://github.com/vivekpal1/vivek',
