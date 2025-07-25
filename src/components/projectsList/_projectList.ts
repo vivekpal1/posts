3084bde47eb7493091fbc884c315d39f9b729e28
vivek pal <31vivekpal@gmail.com>
2025-07-25 10:31:38 +0000

    ft: add some projects

--- src/components/projectsList/_projectList.ts
+++ src/components/projectsList/_projectList.ts
@@ -9,6 +9,24 @@ type Projects<T> = T[]
 
 export const projects: Projects<projectsType> = [
   {
+    projectName: 'MultiRPC',
+    description: 'Solana RPC aggregation system that provides high availability and performance by intelligently routing requests across multiple RPC endpoints.',
+    repoLink: 'https://github.com/vivekpal1/multi-rpc',
+    previewLink: 'https://github.com/vivekpal1/multi-rpc',
+  },
+  {
+    projectName: 'Tide',
+    description: 'A toy lang with self written compiler compatable to compile solana programs in native rust, just wrote to learn things ',
+    repoLink: 'https://github.com/vivekpal1/so-lang',
+    previewLink: 'https://github.com/vivekpal1/so-lang',
+  },
+  {
+    projectName: 'Tide',
+    description: 'Drop-in replacement for Solana geyser plugins to stream data more efficiently.',
+    repoLink: 'https://github.com/wind-network/tide',
+    previewLink: 'https://windnetwork.ai',
+  },
+  {
     projectName: 'Wind Network',
     description: 'Distributed network for solana to index and maintain the storage offchain.',
     repoLink: 'https://github.com/wind-network',
@@ -25,6 +43,12 @@ export const projects: Projects<projectsType> = [
     description: 'Innovating global data management with unprecedented speed and scale.',
     repoLink: 'https://github.com/ipdm-wg/ipdm',
     previewLink: 'https://ipdm.tech',
+  },
+  {
+    projectName: 'Mai: SuperteamVN AI Bot',
+    description: 'A Telegram bot powered by local LLM to assist the Superteam Vietnam Web3 community.',
+    repoLink: 'https://github.com/vivekpal1/superteam-intern',
+    previewLink: 'https://t.me/SuperteamVN_bot',
   },
   {
     projectName: 'Wink',
